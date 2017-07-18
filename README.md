21584e256a90f1c7a55b4604abcc68826991e50b
Alex Barry <aostreetart9@gmail.com>
2017-07-18 02:03:17 -0400

    Updates to include real materials and lighting, as well as orbital controls

---
+++ README.md
@@ -0,0 +1,12 @@
+# NodeJS - ThreeJS Example
+
+To run, simply execute the following commands:
+
+    git clone https://github.com/AO-StreetArt/NodeThree.git
+    cd NodeThree
+    npm start
+
+## Dependencies
+
+Minified version of ThreeJS included, along with 'OrbitControls' from ThreeJS examples.
+License for both included within the js/src/ directory.
--- js/app/main_scene.js
+++ js/app/main_scene.js
@@ -11,33 +11,46 @@ document.body.appendChild( renderer.domElement );
 // Set up a camera
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// Add a cube
-// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// var cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+// Set up some orbital controls
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.target = new THREE.Vector3(0, 0.6, 0);
 
 // Load the model
-var obj = null;
 var loader = new THREE.JSONLoader();
+var obj = null;
 var torus = null;
-loader.load('./resources/json/torus.json', function(obj) {
-  torus = new THREE.Mesh(obj, material)
+loader.load('./resources/json/torus.json', function(obj, materials) {
+  torus = new THREE.Mesh(obj, new THREE.MeshStandardMaterial(materials))
   scene.add(torus);
 });
 
-// Add some ambient lighting
-var light = new THREE.AmbientLight(0xffffff);
-scene.add(light);
+// Add some directional lighting
+var light = new THREE.PointLight( 0xff0000, 10, 100 );
+light.position.set( 0, 2, 0 );
+scene.add( light );
+
+var light2 = new THREE.PointLight( 0x2194ce, 5, 100 );
+light2.position.set( -1, -2, 0 );
+scene.add( light2 );
 
 //Update the camera position so that we can see the cube
 camera.position.z = 20
 
+// Ensure that our window resizes dynamically
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 //Actually set up the animation
 function animate() {
   requestAnimationFrame( animate );
-  //torus.rotation.x += 0.1;
-  //torus.rotation.y += 0.1;
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  controls.update();
   renderer.render( scene, camera );
 }
 animate();
