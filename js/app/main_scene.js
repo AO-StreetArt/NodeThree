1eb2080eff42fe5f2c1fb3440f51fbbc03f811f4
Alex Barry <aostreetart9@gmail.com>
2017-07-18 00:43:26 -0400

    Fixes to get basic torus showing

--- js/app/main_scene.js
+++ js/app/main_scene.js
@@ -1,11 +1,15 @@
 // Set up a Scene
 var scene = new THREE.Scene();
-// Set up a camera
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 // Set up a renderer
 var renderer = new THREE.WebGLRenderer();
+// Ensure that the renderer window is sized correctly
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 //Insert the renderer into the document
 document.body.appendChild( renderer.domElement );
+
+// Set up a camera
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 // Add a cube
 // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -15,9 +19,11 @@ var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
 // Load the model
 var obj = null;
-var loader = new THREE.ObjectLoader();
+var loader = new THREE.JSONLoader();
+var torus = null;
 loader.load('./resources/json/torus.json', function(obj) {
-  scene.add(obj);
+  torus = new THREE.Mesh(obj, material)
+  scene.add(torus);
 });
 
 // Add some ambient lighting
@@ -30,8 +36,8 @@ camera.position.z = 20
 //Actually set up the animation
 function animate() {
   requestAnimationFrame( animate );
-  obj.rotation.x += 0.1;
-  obj.rotation.y += 0.1;
+  //torus.rotation.x += 0.1;
+  //torus.rotation.y += 0.1;
   renderer.render( scene, camera );
 }
 animate();
